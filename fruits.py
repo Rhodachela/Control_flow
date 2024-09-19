f19671ce60f2b854daf6968f308381b2df0f5d62
Rhoda <rhodachela@gmail.com>
2024-09-19 16:12:19 +0300

    Iterating over lists

---
+++ fruits.py
@@ -0,0 +1,20 @@
+fruits = ["Mango", "Banana", "Apple"]
+
+for fruit in fruits:
+    print(fruit)
+
+colors = ("red", "green", "indigo")
+for color in colors:
+    print(color)
+
+message = "Hello, world!"
+for character in message:
+    print(character)
+
+for i in range(1, 5):
+    print(i)
+
+color_list = ["Red", "Violet", "Black", "Pink"]
+color_list[0] = "Purple"
+
+print(color_list)
